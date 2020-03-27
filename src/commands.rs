3781026dcf1c4167bba2432682f907f3ea495fc6
Korbinian Maier <korbinian.maier@tum.de>
2020-03-27 18:10:32 +0100

    Refactoring (#1)
    * Refactor: basically start from scratch
    
    * Storage: some tests
    
    * Commands: started to add functionality
    
    * CI: actions
    
    * Commands: start and stat functionality
    
    * ErrorHandling: use failure and remove custom errors
    
    * doc: improve readme
    
    * License: change to MIT
    
    * Cargo: renamed to stempel, added more info
    
    * README: renamed
    
    * storage: added start date
    
    * Main: improved error handling
    
    * Stats: pretty print
    
    * README: udpate features
    
    * clean up: remove vscode folder
    
    * storage: move module from dir to file in src/
    
    * Cargo: bump version
    
    * Cargo : add description

---
+++ src/commands.rs
@@ -0,0 +1,53 @@
+use chrono::{DateTime, Local};
+use failure::{bail, Error};
+use log::{info, warn};
+use std::path::Path;
+use std::time::Duration;
+
+use crate::storage::*;
+
+pub fn start<P: AsRef<Path>>(storage: P) -> Result<(), Error> {
+    let mut store = WorkStorage::from_file(&storage)?;
+    let now = Duration::new(0, 0);
+    let date: DateTime<Local> = Local::now();
+    store.add_set(WorkSet::new(WorkType::Start, now, date));
+
+    info!("store: {:?}", store);
+    store.write(&storage)?;
+    Ok(())
+}
+
+pub fn stop<P: AsRef<Path>>(storage: P) -> Result<(), Error> {
+    if !storage.as_ref().exists() {
+        bail!(
+            "There is no time storage {:?}, start working first. It creates the file if necessary",
+            storage.as_ref()
+        );
+    }
+    let mut store = WorkStorage::from_file(&storage)?;
+    let start = store.try_start()?;
+    let now: DateTime<Local> = Local::now();
+    let duration: Duration = now.signed_duration_since(start).to_std()?;
+    if duration > Duration::new(26 * 60 * 60, 0) {
+        warn!(
+            "{}, you worked more than a day? It's been {}s",
+            store.name(),
+            duration.as_secs()
+        );
+    }
+
+    store.del_start();
+    store.add_set(WorkSet::new(WorkType::Work, duration, start));
+    store.write(&storage)?;
+    println!(
+        "You worked {}s today. Enjoy your evening :)",
+        duration.as_secs()
+    );
+    Ok(())
+}
+
+pub fn stats<P: AsRef<Path>>(storage: P) -> Result<(), Error> {
+    let store = WorkStorage::from_file(storage)?;
+    println!("{}", store.stats());
+    Ok(())
+}
---
+++ src/storage.rs
@@ -0,0 +1,201 @@
+use chrono::{DateTime, Local};
+use failure::{bail, format_err, Error};
+use std::convert::TryFrom;
+use std::fmt;
+use std::fs::File;
+use std::io::BufReader;
+use std::path::Path;
+use std::time::Duration;
+
+use serde::{Deserialize, Serialize};
+
+#[derive(Serialize, Deserialize, Debug, Eq, PartialEq)]
+pub enum WorkType {
+    Work,
+    Start,
+    Stop,
+}
+
+impl TryFrom<&str> for WorkType {
+    type Error = Error;
+
+    fn try_from(input: &str) -> Result<Self, Self::Error> {
+        match input.to_lowercase().as_str() {
+            "work" => Ok(WorkType::Work),
+            "start" => Ok(WorkType::Start),
+            "stop" => Ok(WorkType::Stop),
+            _ => bail!("Failed to parse {} into WorkType", input),
+        }
+    }
+}
+
+impl fmt::Display for WorkType {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            WorkType::Start => write!(f, "Start"),
+            WorkType::Work => write!(f, " Work"),
+            WorkType::Stop => write!(f, " Stop"),
+        }
+    }
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct WorkSet {
+    ty: WorkType,
+    duration: Duration,
+    start: DateTime<Local>,
+}
+
+impl WorkSet {
+    pub fn new(ty: WorkType, duration: Duration, start: DateTime<Local>) -> Self {
+        WorkSet {
+            ty,
+            duration,
+            start,
+        }
+    }
+}
+
+impl fmt::Display for WorkSet {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        let dur = chrono::Duration::from_std(self.duration).map_err(|_| fmt::Error)?;
+        write!(
+            f,
+            "{} on {}: {:>02}:{:>02} h",
+            self.ty,
+            self.start.format("%d/%m/%Y, %H:%M (%a)"),
+            dur.num_hours(),
+            dur.num_minutes()
+        )
+    }
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct WorkStorage {
+    name: String,
+    work_sets: Vec<WorkSet>,
+}
+
+impl WorkStorage {
+    pub fn from_file<P: AsRef<Path>>(path: P) -> Result<Self, Error> {
+        match File::open(path) {
+            Ok(f) => {
+                let reader = BufReader::new(f);
+                serde_json::from_reader(reader)
+                    .map_err(|e| format_err!("Failed to deserialize json: {}", e))
+            }
+            Err(_) => {
+                println!("Enter your name: ");
+                let mut buffer = String::new();
+                std::io::stdin().read_line(&mut buffer)?;
+                Ok(WorkStorage::new(buffer.trim_end().to_string()))
+            }
+        }
+    }
+
+    fn to_json(&self) -> Result<String, Error> {
+        serde_json::to_string(&self).map_err(|e| format_err!("Failed to serialize to json: {}", e))
+    }
+
+    pub fn write<P: AsRef<Path>>(&self, path: P) -> Result<(), Error> {
+        std::fs::write(path, self.to_json()?)?;
+        Ok(())
+    }
+
+    fn new(name: String) -> Self {
+        WorkStorage {
+            name,
+            work_sets: Vec::new(),
+        }
+    }
+
+    pub fn name(&self) -> &String {
+        &self.name
+    }
+
+    pub fn add_set(&mut self, set: WorkSet) {
+        self.work_sets.push(set);
+    }
+
+    pub fn stats(&self) -> String {
+        self.to_string()
+    }
+
+    pub fn try_start(&self) -> Result<DateTime<Local>, Error> {
+        let start = self
+            .work_sets
+            .iter()
+            .find(|w| w.ty == WorkType::Start)
+            .map(|w| w.start);
+        match start {
+            Some(s) => Ok(s),
+            None => bail!(
+                "You want to stop but you never started, strange work ethics, {}",
+                self.name
+            ),
+        }
+    }
+
+    pub fn del_start(&mut self) {
+        self.work_sets.retain(|w| w.ty != WorkType::Start);
+    }
+}
+
+impl fmt::Display for WorkStorage {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "{}:", self.name)?;
+        for set in self.work_sets.iter() {
+            write!(f, "\n\t{}", set)?;
+        }
+        Ok(())
+    }
+}
+
+#[test]
+fn worktype_parses() {
+    let w = "work";
+    assert_eq!(WorkType::try_from(w).unwrap(), WorkType::Work);
+    let w = "start";
+    assert_eq!(WorkType::try_from(w).unwrap(), WorkType::Start);
+    let w = "stop";
+    assert_eq!(WorkType::try_from(w).unwrap(), WorkType::Stop);
+    let w = "something";
+    assert!(WorkType::try_from(w).is_err());
+}
+
+#[test]
+fn serde_ok() {
+    let store_raw = r#"{
+        "name": "test",
+        "work_sets": [
+            {
+                "ty": "Work",
+                "duration": {"secs": 2, "nanos": 0},
+                "start": "2020-03-27T10:22:12.755844511+00:00"
+            }
+        ]
+    }"#;
+    let mut store: WorkStorage = serde_json::from_str(store_raw).expect("Failed to deserialize");
+    assert_eq!(store.name, "test");
+    assert_eq!(store.work_sets.first().unwrap().ty, WorkType::Work);
+    assert_eq!(
+        store.work_sets.first().unwrap().duration,
+        std::time::Duration::from_secs(2)
+    );
+
+    let store_ser = serde_json::to_string(&store).expect("Failed to serialize");
+    let dt: DateTime<Local> = DateTime::parse_from_rfc3339("2020-03-27T10:22:12.755844511+00:00")
+        .unwrap()
+        .into();
+    store.work_sets[0].start = dt;
+    assert_eq!(store_raw.replace('\n', "").replace(' ', ""), store_ser);
+    assert_eq!(store_ser, store.to_json().expect("Failed to serialize"));
+}
+
+#[test]
+fn serde_throws() {
+    let store_raw = r#"{
+        "name": "test",
+    }"#;
+    assert!(serde_json::from_str::<WorkStorage>(store_raw).is_err());
+}
