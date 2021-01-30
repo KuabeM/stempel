352be7e96ae3654a1c442725299c3f180165fc7d
Korbinian Maier <korbinian.maier@posteo.de>
2021-01-30 22:37:49 +0100

    refactor/control: use new database

--- src/commands/stats.rs
+++ src/commands/stats.rs
@@ -2,7 +2,7 @@
 //!
 //! The main entry point is `stats` which then further decides what to do.
 
-use crate::commands::control::time_from_duration;
+//use crate::commands::control::time_from_duration;
 use crate::month::Month;
 use crate::storage::{WorkStorage, WorkType};
 
@@ -40,36 +40,36 @@ pub fn stats<P: AsRef<Path>>(storage: P, month: Option<Month>) -> Result<(), Err
 fn all_monthly_stats<P: AsRef<Path>>(storage: P) -> Result<(), Error> {
     let store = WorkStorage::from_file(storage)?;
     let grouped_work = store.split_work_by_year();
-    for year in grouped_work() {
-        for
+    for (year, work) in grouped_work {
+        println!("{}, {:?}", year, work);
     }
     let years = store.years();
     let weeks = store.weeks();
     info!("Here are your stats sorted by years, {}:", store.name());
-    for y in years {
-        info!("{}", y);
-        let work_in_year = store.work_in_year(y);
-        for w in work_in_year {
-            println!(
-                "{} {: >2}{}",
-                "Month".green(),
-                a.to_string().green(),
-                ":".green()
-            );
-            for w in &weeks {
-                let work_per_w: Duration = work_per_m
-                    .filter(|s| &s.start.date().format("%W").to_string() == w)
-                    .filter(|s| s.ty == WorkType::Work)
-                    .work_sets
-                    .iter()
-                    .fold(Duration::new(0, 0), |acc, d| acc + d.duration);
-                if work_per_w.as_nanos() > 0 {
-                    let (h, min) = time_from_duration(work_per_w);
-                    println!(" Week {}: {: >4}:{:02}h", w, h, min);
-                }
-            }
-        }
-    }
+    // for y in years {
+    //     info!("{}", y);
+    //     let work_in_year = store.work_in_year(y);
+    //     for w in work_in_year {
+    //         println!(
+    //             "{} {: >2}{}",
+    //             "Month".green(),
+    //             a.to_string().green(),
+    //             ":".green()
+    //         );
+    //         for w in &weeks {
+    //             let work_per_w: Duration = work_per_m
+    //                 .filter(|s| &s.start.date().format("%W").to_string() == w)
+    //                 .filter(|s| s.ty == WorkType::Work)
+    //                 .work_sets
+    //                 .iter()
+    //                 .fold(Duration::new(0, 0), |acc, d| acc + d.duration);
+    //             if work_per_w.as_nanos() > 0 {
+    //                 let (h, min) = time_from_duration(work_per_w);
+    //                 println!(" Week {}: {: >4}:{:02}h", w, h, min);
+    //             }
+    //         }
+    //     }
+    // }
     if let Ok(s) = store.try_start() {
         println!(" {}", s);
     }
@@ -98,7 +98,7 @@ fn monthly_stats<P: AsRef<Path>>(storage: P, month: Month) -> Result<(), Error> 
             .iter()
             .fold(Duration::new(0, 0), |acc, d| acc + d.duration);
         if work_per_w.as_nanos() > 0 {
-            let (h, min) = time_from_duration(work_per_w);
+            let (h, min) = (10, 10);//time_from_duration(work_per_w);
             println!(" Week {}: {: >4}:{:02}h", w, h, min);
         }
     }
